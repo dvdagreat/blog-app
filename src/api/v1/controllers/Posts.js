b082c6158b039ad87a194f4f3cbb03957cd678bd
Divyesh Ladva <ladoodivyesh@gmail.com>
2022-06-17 18:04:30 +0530

    Setup basic Post controller and routing

---
+++ src/api/v1/controllers/Posts.js
@@ -0,0 +1,8 @@
+class PostController {
+  static async createPost(req, res, dependencies) {}
+  static async getPosts(req, res, dependencies) {}
+  static async updatePost(req, res, dependencies) {}
+  static async deletePost(req, res, dependencies) {}
+}
+
+export default new PostController();
--- src/api/v1/routes/Posts.js
+++ src/api/v1/routes/Posts.js
@@ -1,7 +1,25 @@
 import { Router } from "express";
 
-const PostRouter = () => {
+import PostController from "../controllers/Posts";
+
+const PostRouter = (dependencies) => {
   const router = Router();
+
+  router.get("/", (req, res) => {
+    PostController.getPosts(req, res, dependencies);
+  });
+
+  router.post("/", (req, res) => {
+    PostController.createPost(req, res, dependencies);
+  });
+
+  router.put("/:id", (req, res) => {
+    PostController.updatePost(req, res, dependencies);
+  });
+
+  router.delete("/:id", (req, res) => {
+    PostController.deletePost(req, res, dependencies);
+  });
 
   return router;
 };
