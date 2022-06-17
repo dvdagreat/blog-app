7dc6a8d3665b9af012bce149a7fcd44ac84fbfed
Divyesh Ladva <ladoodivyesh@gmail.com>
2022-06-17 18:32:42 +0530

    Setup Posts schema & model

---
+++ src/database/models/Posts.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const PostSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    published_date: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["published", "unpublished"],
+      default: "published",
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Posts", PostSchema);
