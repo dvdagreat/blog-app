d11363d8edea49fb8dfab70f5a85922c3b005ed9
Divyesh Ladva <ladoodivyesh@gmail.com>
2022-06-17 20:15:25 +0530

    Add mongoose code for Users database wrapper

--- src/database/abstraction/Users.js
+++ src/database/abstraction/Users.js
@@ -2,6 +2,24 @@ class UsersDatabaseWrapper {
   constructor(model) {
     this.model = model;
   }
+
+  async findByUsername(email) {
+    try {
+      const user = await this.model.findOne({ email });
+      return user;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async createNew(user) {
+    try {
+      const user = new this.model(user);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
 }
 
 export default UsersDatabaseWrapper;
